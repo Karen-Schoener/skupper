f4220f0f6ce9b9ac4800a180880a58efc5c6728a
Karen-Schoener <100308664+Karen-Schoener@users.noreply.github.com>
2024-10-16 16:56:31 -0400

    tc: upgrade reads skupper-network-status.yaml

--- cmd/upgrade/debug_dump_networkstatus.go
+++ cmd/upgrade/debug_dump_networkstatus.go
@@ -1,46 +1,78 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"path/filepath"
 
-	"fmt"
-	"log"
-	//"os"
 	"gopkg.in/yaml.v3"
 )
 
 type NetworkStatusDataUnmarshaled struct {
-	Data NetworkStatus `yaml:"data"`
+	Data struct {
+		NetworkStatus string `yaml:"NetworkStatus"` // This is the stringified JSON
+	} `yaml:"data"`
 }
+
 type NetworkStatus struct {
-	// TODO: add addresses
-	//SiteStatus []SiteStatus `yaml:"siteStatus"`
-	Site []interface{} `yaml:"siteStatus"`
-	//RouterStatus []RouterStatus `yaml:"routerStatus"`
-	//RouterStatus interface{} `yaml:"routerStatus"`
+	Addresses  interface{}  `json:"addresses"`  // Change yaml to json since this is a JSON string
+	SiteStatus []SiteStatus `json:"siteStatus"` // List of sites
 }
 
 type SiteStatus struct {
-	Site interface{} `yaml:"siteStatus"`
-	//RouterStatus []RouterStatus `yaml:"routerStatus"`
-	RouterStatus interface{} `yaml:"routerStatus"`
+	Site         Site           `json:"site"`
+	RouterStatus []RouterStatus `json:"routerStatus"`
 }
 
-type NetworkStatusDataUnmarshaled_1 struct {
-	Data map[string]interface{} `yaml:"data"`
+type Site struct {
+	RecType     string `json:"recType"`
+	Identity    string `json:"identity"`
+	StartTime   int64  `json:"startTime"`
+	EndTime     int64  `json:"endTime"`
+	Source      string `json:"source"`
+	Platform    string `json:"platform"`
+	Name        string `json:"name"`
+	Namespace   string `json:"nameSpace"`
+	SiteVersion string `json:"siteVersion"`
+	Policy      string `json:"policy"`
 }
 
-type NetworkStatusDataUnmarshaled_2 struct {
-	Data map[string]NetworkStatus_2 `yaml:"data"`
+type RouterStatus struct {
+	Router     Router      `json:"router"`
+	Links      []Link      `json:"links,omitempty"`
+	Listeners  interface{} `json:"listeners"`
+	Connectors interface{} `json:"connectors"`
 }
-type NetworkStatus_2 struct {
-	Addresses []interface{} `yaml:"addresses"`
-	SiteStatus []interface{} `yaml:"siteStatus"`
-	//SiteStatus []SiteStatus_2 `yaml:"siteStatus"`
+
+type Router struct {
+	RecType      string `json:"recType"`
+	Identity     string `json:"identity"`
+	Parent       string `json:"parent"`
+	StartTime    int64  `json:"startTime"`
+	EndTime      int64  `json:"endTime"`
+	Source       string `json:"source"`
+	Name         string `json:"name"`
+	Namespace    string `json:"namespace,omitempty"`
+	Mode         string `json:"mode"`
+	ImageName    string `json:"imageName,omitempty"`
+	ImageVersion string `json:"imageVersion"`
+	Hostname     string `json:"hostname"`
+	BuildVersion string `json:"buildVersion"`
 }
-type SiteStatus_2 struct {
-	SiteStatus []map[string]interface{} `yaml:"site"`
+
+type Link struct {
+	RecType   string `json:"recType"`
+	Identity  string `json:"identity"`
+	Parent    string `json:"parent"`
+	StartTime int64  `json:"startTime"`
+	EndTime   int64  `json:"endTime"`
+	Source    string `json:"source"`
+	Mode      string `json:"mode"`
+	Name      string `json:"name"`
+	Direction string `json:"direction"`
+	Cost      int    `json:"cost"` // TODO test with cost populated
 }
 
 func readNetworkStatus(path string) (*NetworkStatus, error) {
@@ -58,28 +90,39 @@ func readNetworkStatus(path string) (*NetworkStatus, error) {
 
 	log.Printf("TMPDBG: readNetworkStatus: data=%+v", string(data))
 
-	var tmpdata_0 map[string]interface{}
-	err = yaml.Unmarshal(data, &tmpdata_0)
-	log.Printf("TMPDBG: readNetworkStatus: TMPDBG: case 00.1: err=%+v", err)
-	log.Printf("TMPDBG: readNetworkStatus: TMPDBG: case 00.2: tmpdata_0=%+v", tmpdata_0)
-
-	var tmpdata_1 NetworkStatusDataUnmarshaled_1
-	err = yaml.Unmarshal(data, &tmpdata_1)
-	log.Printf("TMPDBG: readNetworkStatus: TMPDBG: case 10.1: err=%+v", err)
-	log.Printf("TMPDBG: readNetworkStatus: TMPDBG: case 10.2: tmpdata_1=%+v", tmpdata_1)
-
-	var tmpdata_2 NetworkStatusDataUnmarshaled_2
-	err = yaml.Unmarshal(data, &tmpdata_2)
-	log.Printf("TMPDBG: readNetworkStatus: TMPDBG: case 20.1: err=%+v", err)
-	log.Printf("TMPDBG: readNetworkStatus: TMPDBG: case 20.2: tmpdata_2=%+v", tmpdata_2)
-
+	// Unmarshal YAML into NetworkStatusDataUnmarshaled to extract the JSON string
 	var networkStatusDataUnmarshaled NetworkStatusDataUnmarshaled
 	err = yaml.Unmarshal(data, &networkStatusDataUnmarshaled)
 	if err != nil {
-		log.Printf("TMPDBG: readNetworkStatus: err=%+v", err)
+		log.Printf("Error unmarshalling YAML: %+v", err)
 		return nil, fmt.Errorf("Error unmarshalling file %s data: %v", filename, err)
 	}
-	log.Printf("TMPDBG: readNetworkStatus: networkStatusDataUnmarshaled=%+v", networkStatusDataUnmarshaled)
+
+	log.Printf("TMPDBG: NetworkStatus JSON: %s", networkStatusDataUnmarshaled.Data.NetworkStatus)
+
+	// Unmarshal the JSON part of the NetworkStatus
+	err = json.Unmarshal([]byte(networkStatusDataUnmarshaled.Data.NetworkStatus), networkStatus)
+	if err != nil {
+		log.Printf("Error unmarshalling NetworkStatus JSON: %+v", err)
+		return nil, fmt.Errorf("Error unmarshalling NetworkStatus JSON: %v", err)
+	}
+
+	log.Printf("TMPDBG: Unmarshalled network status: %+v", networkStatus)
+
+	DumpNetworkStatus(networkStatus)
 
 	return networkStatus, nil
+}
+
+func DumpNetworkStatus(networkStatus *NetworkStatus) {
+	for i, siteStatus := range networkStatus.SiteStatus {
+		log.Printf("TMPDBG: case 50: i=%+v, siteStatus=%+v", i, siteStatus)
+		for j, routerStatus := range siteStatus.RouterStatus {
+			log.Printf("TMPDBG: case 51: i=%+v, j=%+v, routerStatus=%+v", i, j, routerStatus)
+			log.Printf("TMPDBG: case 52: i=%+v, j=%+v, len(routerStatus.Links)=%+v", i, j, len(routerStatus.Links))
+			for k, link := range routerStatus.Links {
+				log.Printf("TMPDBG: case 53: i=%+v, j=%+v, k=%+v, link=%+v", i, j, k, link)
+			}
+		}
+	}
 }
