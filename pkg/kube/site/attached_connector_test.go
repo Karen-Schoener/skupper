4a78bb3bd4eb5756e1ec836e359644561186fe77
Karen Schoener <karen.schoener@ibm.com>
2024-11-18 18:02:37 -0500

    [v2] add attached connector tests to pkg/kube/site
    Updating unit test for attachedConnectorUpdated

--- pkg/kube/site/attached_connector_test.go
+++ pkg/kube/site/attached_connector_test.go
@@ -7,9 +7,52 @@ import (
 	skupperv2alpha1 "github.com/skupperproject/skupper/pkg/apis/skupper/v2alpha1"
 	"github.com/skupperproject/skupper/pkg/kube"
 	"github.com/skupperproject/skupper/pkg/site"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	//"gotest.tools/assert"
+
+	"log"
+
+	fakeclient "github.com/skupperproject/skupper/internal/kube/client/fake"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
+func NewMockSite(namespace string) *Site {
+
+	site, err := newSiteMocks(namespace, nil, nil, "", false)
+
+	log.Printf("TMPDBG: NewMockSite: err=%+v", err)
+
+	//assert.Assert(t, err)
+
+	return site
+}
+
+func NewMockController(namespace string) (*kube.Controller, error) {
+
+	k8sObjects := []runtime.Object{}
+	skupperObjects := []runtime.Object{}
+	fakeSkupperError := ""
+
+	client, err := fakeclient.NewFakeClient(namespace, k8sObjects, skupperObjects, fakeSkupperError)
+	if err != nil {
+		return nil, err
+	}
+
+	controller := kube.NewController(namespace, client)
+
+	return controller, nil
+}
+
 func TestExtendedBindings_attachedConnectorUpdated(t *testing.T) {
+	log.SetFlags(log.LstdFlags | log.Llongfile) // for full file path and line numbers
+	initController := func(namespace string) *kube.Controller {
+		controller, err := NewMockController(namespace)
+		if err != nil {
+			t.Fatalf("Failed to initialzie controller: %v", err)
+		}
+		return controller
+	}
 	type fields struct {
 		bindings   *site.Bindings
 		connectors map[string]*AttachedConnector
@@ -27,9 +70,32 @@ func TestExtendedBindings_attachedConnectorUpdated(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "No matching AttachedConnectorAnchor in site namespace",
+			fields: fields{
+				bindings: &site.Bindings{
+				},
+				connectors: map[string]*AttachedConnector{},
+				controller: initController("test"),
+				site:       NewMockSite("test"),
+				logger: slog.New(slog.Default().Handler()).With(
+					slog.String("component", "kube.site.attached_connector"),
+				),
+			},
+			args: args{
+				name: "backend",
+				definition: &skupperv2alpha1.AttachedConnector{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "backend",
+						Namespace: "test-anchor",
+					},
+				},
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
+		log.Printf("TMPDBG: binding_test: tt.name=%+v", tt.name)
 		t.Run(tt.name, func(t *testing.T) {
 			b := &ExtendedBindings{
 				bindings:   tt.fields.bindings,
