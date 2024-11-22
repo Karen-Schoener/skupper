45ed2aa20e40f82c137001e192b59a718011545d
Karen Schoener <karen.schoener@ibm.com>
2024-11-22 17:32:06 -0500

    snapshot updating test to verify configmap
    Changes required to use 1 fakeclient when creating the
    controller and extendedbindings.
    Necessary for test to verify router config is correct in configmap.

--- pkg/kube/site/attached_connector_test.go
+++ pkg/kube/site/attached_connector_test.go
@@ -18,32 +18,58 @@ import (
 	"github.com/skupperproject/skupper/pkg/kube/certificates"
 	"github.com/skupperproject/skupper/pkg/kube/securedaccess"
 	corev1 "k8s.io/api/core/v1"
-        //"github.com/skupperproject/skupper/internal/kube/client"
+	//"github.com/skupperproject/skupper/internal/kube/client"
 
-
-        "context"
-        "github.com/skupperproject/skupper/api/types"
-        //"github.com/skupperproject/skupper/internal/kube/client"
-        "k8s.io/apimachinery/pkg/api/errors"
-        "k8s.io/client-go/kubernetes"
-
+	"context"
+	"github.com/skupperproject/skupper/api/types"
+	//"github.com/skupperproject/skupper/internal/kube/client"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/client-go/kubernetes"
 )
 
-func getTestSiteCr() *skupperv2alpha1.Site {
-		return &skupperv2alpha1.Site{
-			ObjectMeta: v1.ObjectMeta{
-				Name:      "test",
-				Namespace: "test",
-				//UID:       "8a96ffdf-403b-4e4a-83a8-97d3d459adb6",
-			},
-			//Spec: skupperv2alpha1.SiteSpec{
-				//DefaultIssuer: "skupper-spec-issuer-ca",
-			//},
-			//Status: skupperv2alpha1.SiteStatus{
-				//DefaultIssuer: "skupper-status-issuer-ca",
-			//},
-		}
+func initAttachedConnectorAnchorCr() *skupperv2alpha1.AttachedConnectorAnchor {
+	return &skupperv2alpha1.AttachedConnectorAnchor{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "backend",
+			Namespace: "test",
+		},
+		Spec: skupperv2alpha1.AttachedConnectorAnchorSpec{
+			RoutingKey:         "backend",
+			ConnectorNamespace: "site-with-backend-pods",
+		},
 	}
+}
+
+func initAttachedConnectorCr() *skupperv2alpha1.AttachedConnector {
+	return &skupperv2alpha1.AttachedConnector{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "backend",
+			Namespace: "site-with-backend-pods",
+			UID:       "00000000-0000-0000-0000-000000000002",
+		},
+		Spec: skupperv2alpha1.AttachedConnectorSpec{
+			Port:          8080,
+			Selector:      "app=backend",
+			SiteNamespace: "test",
+		},
+	}
+}
+
+func getTestSiteCr() *skupperv2alpha1.Site {
+	return &skupperv2alpha1.Site{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "test",
+			Namespace: "test",
+			//UID:       "8a96ffdf-403b-4e4a-83a8-97d3d459adb6",
+		},
+		//Spec: skupperv2alpha1.SiteSpec{
+		//DefaultIssuer: "skupper-spec-issuer-ca",
+		//},
+		//Status: skupperv2alpha1.SiteStatus{
+		//DefaultIssuer: "skupper-status-issuer-ca",
+		//},
+	}
+}
 
 func newSiteMocksCopy(name string, namespace string, k8sObjects []runtime.Object, skupperObjects []runtime.Object) (*Site, error) {
 	siteCr := getTestSiteCr()
@@ -89,10 +115,10 @@ func NewMockSite(name string, namespace string, k8sObjects []runtime.Object, sku
 }
 
 func NewMockController(namespace string, k8sObjects []runtime.Object, skupperObjects []runtime.Object) (*kube.Controller, error) {
-        client, err := fakeclient.NewFakeClient(namespace, k8sObjects, skupperObjects, "")
-        if err != nil {
-                return nil, err
-        }
+	client, err := fakeclient.NewFakeClient(namespace, k8sObjects, skupperObjects, "")
+	if err != nil {
+		return nil, err
+	}
 
 	controller := kube.NewController(namespace, client)
 
@@ -101,39 +127,13 @@ func NewMockController(namespace string, k8sObjects []runtime.Object, skupperObj
 
 func TestExtendedBindings_attachedConnectorUpdated(t *testing.T) {
 	log.SetFlags(log.LstdFlags | log.Llongfile) // for full file path and line numbers
-	initAttachedConnectorCr := func() *skupperv2alpha1.AttachedConnector {
-		return &skupperv2alpha1.AttachedConnector{
-			ObjectMeta: v1.ObjectMeta{
-				Name:      "backend",
-				Namespace: "site-with-backend-pods",
-				UID:       "00000000-0000-0000-0000-000000000002",
-			},
-			Spec: skupperv2alpha1.AttachedConnectorSpec{
-				Port:          8080,
-				Selector:      "app=backend",
-				SiteNamespace: "test",
-			},
-		}
-	}
 	initAttachedConnectorCrSpecChanged := func() *skupperv2alpha1.AttachedConnector {
 		a := initAttachedConnectorCr()
 		a.Spec.Port = 8081
 		return a
 	}
-	initAttachedConnectorAnchorCr := func() *skupperv2alpha1.AttachedConnectorAnchor {
-		return &skupperv2alpha1.AttachedConnectorAnchor{
-			ObjectMeta: v1.ObjectMeta{
-				Name:      "backend",
-				Namespace: "test",
-			},
-			Spec: skupperv2alpha1.AttachedConnectorAnchorSpec{
-				RoutingKey:         "backend",
-				ConnectorNamespace: "site-with-backend-pods",
-			},
-		}
-	}
-        initController := func(namespace string, k8sObjects []runtime.Object, skupperObjects []runtime.Object) *kube.Controller {
-                controller, err := NewMockController(namespace, k8sObjects, skupperObjects)
+	initController := func(namespace string, k8sObjects []runtime.Object, skupperObjects []runtime.Object) *kube.Controller {
+		controller, err := NewMockController(namespace, k8sObjects, skupperObjects)
 		assert.Assert(t, err)
 		if err != nil {
 			t.Fatalf("Failed to initialzie controller: %v", err)
@@ -141,11 +141,13 @@ func TestExtendedBindings_attachedConnectorUpdated(t *testing.T) {
 		return controller
 	}
 	type fields struct {
-		bindings   *site.Bindings
-		connectors map[string]*AttachedConnector
-		controller *kube.Controller
-		site       *Site
-		logger     *slog.Logger
+		k8sObjects     []runtime.Object
+		skupperObjects []runtime.Object
+		bindings       *site.Bindings
+		connectors     map[string]*AttachedConnector
+		controller     *kube.Controller
+		site           *Site
+		logger         *slog.Logger
 	}
 	type testInitSteps struct {
 		setWatcher  bool
@@ -169,6 +171,11 @@ func TestExtendedBindings_attachedConnectorUpdated(t *testing.T) {
 				recoverSite: true,
 			},
 			fields: fields{
+				k8sObjects: []runtime.Object{},
+				skupperObjects: []runtime.Object{
+					initAttachedConnectorCr(),
+					initAttachedConnectorAnchorCr(),
+				},
 				bindings: &site.Bindings{},
 				connectors: map[string]*AttachedConnector{
 					// Attached connector with anchor
@@ -178,15 +185,9 @@ func TestExtendedBindings_attachedConnectorUpdated(t *testing.T) {
 							//"site-with-backend-pods": initAttachedConnectorCr(),
 							"site-with-backend-pods": initAttachedConnectorCrSpecChanged(),
 						},
-						// parent is set in setParent api...
-						// parent: &ExtendedBindings{ // TODO: circular weirdness
-						//		logger: slog.New(slog.Default().Handler()).With(
-						//			slog.String("component", "kube.site.attached_connector"),
-						//			),
-						//},
 					},
 				},
-				controller: initController("test", 
+				controller: initController("test",
 					[]runtime.Object{},
 					[]runtime.Object{
 						initAttachedConnectorCr(),
@@ -233,27 +234,26 @@ func TestExtendedBindings_attachedConnectorUpdated(t *testing.T) {
 				t.Errorf("ExtendedBindings.attachedConnectorUpdated() error = %v, wantErr %v", err, tt.wantErr)
 			}
 
-			namespace := "test" 
-                	cm, err := readConfigMap(context.Background(), namespace, types.TransportConfigMapName, tt.fields.site.controller.GetKubeClient())
+			namespace := "test"
+			cm, err := readConfigMap(context.Background(), namespace, types.TransportConfigMapName, tt.fields.site.controller.GetKubeClient())
 			log.Printf("TMPDBG: after readConfigMap: err=%+v", err)
 			log.Printf("TMPDBG: after readConfigMap: cm=%+v", cm)
-
 
 		})
 	}
 }
 
 func readConfigMap(ctx context.Context, namespace string, name string, kubeClient kubernetes.Interface) (*corev1.ConfigMap, error) {
-//func readConfigMap(ctx context.Context, namespace string, name string, client *client.KubeClient) (*corev1.ConfigMap, error) {
-//func readConfigMap(ctx context.Context, namespace string, name string, kubeClient kubernetes.Interface) (*corev1.ConfigMap, error) {
-        //kubeClient := client.GetKubeClient()
-        cm, err := kubeClient.CoreV1().ConfigMaps(namespace).Get(ctx, name, v1.GetOptions{})
-        if errors.IsNotFound(err) {
-                return nil, nil
-        } else if err != nil {
-                return nil, err
-        }
-        return cm, err
+	//func readConfigMap(ctx context.Context, namespace string, name string, client *client.KubeClient) (*corev1.ConfigMap, error) {
+	//func readConfigMap(ctx context.Context, namespace string, name string, kubeClient kubernetes.Interface) (*corev1.ConfigMap, error) {
+	//kubeClient := client.GetKubeClient()
+	cm, err := kubeClient.CoreV1().ConfigMaps(namespace).Get(ctx, name, v1.GetOptions{})
+	if errors.IsNotFound(err) {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	return cm, err
 }
 
 func updateParent(b *ExtendedBindings) {
